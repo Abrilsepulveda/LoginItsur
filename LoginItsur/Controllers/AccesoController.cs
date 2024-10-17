fbfb1453b5b82c3d9662bc8bf16020311a5cad94
Abril <sepulveda.abril.epet20@gmail.com>
2024-10-17 10:25:08 -0300

    Agregué el archivo AccesoController.cs

---
+++ LoginItsur/Controllers/AccesoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginItsur.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
