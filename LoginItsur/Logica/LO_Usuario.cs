b26fde50dc3556169d62182239ffce71246f723f
Abril <sepulveda.abril.epet20@gmail.com>
2024-10-17 10:10:06 -0300

    Hice cambios en LO_Usuarios.cs

--- LoginItsur/Logica/LO_Usuario.cs
+++ LoginItsur/Logica/LO_Usuario.cs
@@ -2,6 +2,8 @@
 using LoginItsur.Models;
 using System.Data.SqlClient;
 using System.Data;
+using LoginItsur.services;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace LoginItsur.Logica
 {
@@ -12,7 +14,30 @@ namespace LoginItsur.Logica
 
             Usuarios objeto = new Usuarios();
 
-            using(SqlConnection conexion = new SqlConnection("Data source=(mercurio) ; Initial Catalog=Db_ITsur_CSharp; "))
+            using (SqlConnection conexion = new SqlConnection("Data source=(mercurio) ; Initial Catalog=Db_ITsur_CSharp; "))
+
+            string query = "select UserId, Contrasenia, Administrador, CambiarContrasenia from Usuarios where UserID = @puserID and Contrasenia = @pcontrasenia";
+            SqlCommand cmd = new SqlCommand(query, conexion);
+            cmd.Parameters.AddWithValue("@puserID", UserID);
+            cmd.Parameters.AddWithValue("@pcontrasenia", Contrasenia);
+            cmd ComandType = ComandType.Text;
+
+            ConexionDB.Open();
+
+            using(SqlDataReader dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    objeto = new Usuarios()
+                    {
+                        UserID = dr["Usuario"].ToString(),
+                        Contrasenia = dr["Contraseña"].ToString(),
+                        Administrador = dr["Administrador"].ToString(),
+                        CambiarContrasenia = dr["Cambiar Contraseña"],
+
+                    }
+                }
+            }
 
             return objeto;
         }
